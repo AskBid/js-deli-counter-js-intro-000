8f088b115daffa94fa4cb8798195dc1ffe028c3c
sergio marelli <sergio.mrll@gmail.com>
2018-09-30 10:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function takeANumber(currentLine, newPerson) {
     return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
 }
 
-function nowServing() {
-    
+function nowServing(line) {
+    if (line.length > 0) {
+        return `Currently serving Steven ${line.shift()}.`
+    } else {
+        return "The line is currently empty."
+    }
 }
