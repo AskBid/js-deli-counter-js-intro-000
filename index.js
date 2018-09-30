dd2fd83d05a40e3e1e5644a54c5f59b23b9cfca7
sergio marelli <sergio.mrll@gmail.com>
2018-09-30 10:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(currentLine, newPerson) {
     currentLine.push(newPerson)
     return `Welcome, ${newPerson}. You are number ${currentLine.length} in line.`
 }
+
+function nowServing() {
+    
+}
